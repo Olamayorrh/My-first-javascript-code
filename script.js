7991cba4e84b01c9cc11e5b2a11a2cacac4aec43
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2022-06-24 06:09:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Welcome to my first javascript code!!!")
+let Name = " My name is Olamayowa Olaniyan"
+let Height = " I am 5.2 inches tall"
+let Country = " I am from Nigeria"
+
+console.log(Name);
+console.log(Height);
+console.log(Country);
